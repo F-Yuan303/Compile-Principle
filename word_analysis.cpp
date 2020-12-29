ed2f0a83a1ed02d1064c90b73ac4aa877c565189
F-Yuan303 <1261661322@qq.com>
2020-12-29 08:43:06 +0800

    12.28早8:42的更改

--- word_analysis.cpp
+++ word_analysis.cpp
@@ -4,6 +4,61 @@
 #include<string>
 using namespace std;
 
+
+
+struct table {
+	string identity;    //定义名称
+	string type;        //类型-const等
+	int val;            //值
+	//int addr;         //偏移量
+};
+
+struct tablelink {
+	vector<table> level_table;
+	//int level;
+}Tablelink[200];        //符号表
+
+int find(string name) {//查找符号
+	for (int i = 0; i < Tablelink[0].level_table.size(); i++)
+	{
+		if (Tablelink[0].level_table[i].identity == name)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void add(string name, string type, int val) {//添加符号
+	table new_symbol;
+	new_symbol.identity = name;
+	new_symbol.type = type;
+	new_symbol.val = val;
+	Tablelink[0].level_table.push_back(new_symbol);
+}
+
+void del(string name) {//删除符号
+	for (int i = 0; i < Tablelink[0].level_table.size(); i++)
+	{
+		if (Tablelink[0].level_table[i].identity == name)
+		{
+			Tablelink[0].level_table.erase(Tablelink[0].level_table.begin() + i);
+		}
+	}
+}
+
+void display(){
+    cout << "                  **符号表**                  " << endl;
+    cout << "---------------------------------------------" << endl;
+    cout << "     符号名     ｜     类型     ｜      值      " << endl;
+    cout << "---------------------------------------------" << endl;
+    for (int i=0; i< Tablelink[0].level_table.size(); i++){
+        cout << "       " << Tablelink[0].level_table[i].identity << "            " << Tablelink[0].level_table[i].type << "             " << Tablelink[0].level_table[i].val;
+        cout << endl;
+    }
+}
+
+
 string pretreat()
 {
 	fstream file;
@@ -163,58 +218,6 @@ int judge_id(vector<Category> cate, string str)
 	return flag;
 }
 
-
-struct table {
-	string identity;    //定义名称
-	string type;        //类型-const等
-	int val;            //值
-	//int addr;         //偏移量
-};
-
-struct tablelink {
-	vector<table> level_table;
-	//int level;
-}Tablelink[200];        //符号表
-
-int find(string name) {//查找符号
-	for (int i = 0; i < Tablelink[0].level_table.size(); i++)
-	{
-		if (Tablelink[0].level_table[i].identity == name)
-		{
-			return i;
-		}
-	}
-	return -1;
-}
-
-void add(string name, string type, int val) {//添加符号
-	table new_symbol;
-	new_symbol.identity = name;
-	new_symbol.type = type;
-	new_symbol.val = val;
-	Tablelink[0].level_table.push_back(new_symbol);
-}
-
-void del(string name) {//删除符号
-	for (int i = 0; i < Tablelink[0].level_table.size(); i++)
-	{
-		if (Tablelink[0].level_table[i].identity == name)
-		{
-			Tablelink[0].level_table.erase(Tablelink[0].level_table.begin() + i);
-		}
-	}
-}
-
-void display(){
-    cout << "                  **符号表**                  " << endl;
-    cout << "---------------------------------------------" << endl;
-    cout << "     符号名     ｜     类型     ｜      值      " << endl;
-    cout << "---------------------------------------------" << endl;
-    for (int i=0; i< Tablelink[0].level_table.size(); i++){
-        cout << "       " << Tablelink[0].level_table[i].identity << "            " << Tablelink[0].level_table[i].type << "             " << Tablelink[0].level_table[i].val;
-        cout << endl;
-    }
-}
 
 void create_table(vector<Category> cate, string source_str, vector<Category> &source_cate)
 {
