7b58777ac3d52dd4b4d830045f39e1c0061f7cee
F-Yuan303 <1261661322@qq.com>
2020-12-29 10:14:57 +0800

    12.29早10的提交

---
+++ grammar_analysis.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+
+
+void first_list(){
+
+}
+
+void follow_list(){
+
+}
+
+
+
+
+
+
+
+
+int main(){
+    fstream grammar;
+    grammar.open("BNF.txt");
+    
+    grammar.close();
+
+    return 0;
+}
